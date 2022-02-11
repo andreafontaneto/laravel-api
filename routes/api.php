f3635ca1944844f80c20f1a45ebefacd8402d612
andreafontaneto <andrea.fontaneto@gmail.com>
2022-02-11 16:51:47 +0100

    test json structure

--- routes/api.php
+++ routes/api.php
@@ -21,10 +21,34 @@ LE ROTTE con middleware non servono perchè la vista essendo pubblica non ha bis
 // quindi faccio un altra rotta normale
 // tutte le rotte inserite qui in "api.php" avranno come prefisso "api/..." (es. api/prova)
 Route::get('prova', function(){
-    return response()->json(
-        [
-            "name" => "Andrea", 
-            "lastname" => "Fontaneto"
+
+    // posso salvare il mio array dentro $data o restituirlo direttamente tramite ->json([...])
+    // $data = [
+    //     "name" => "Andrea", 
+    //     "lastname" => "Fontaneto"
+    // ];
+
+    // non c'è relazione tra il nome della variabile e quello che viene stampato
+    // viene stampato direttamente l'array
+    $user = [
+        "name" => "Andrea", 
+        "lastname" => "Fontaneto",
+        "hobbies" => [
+            'coding',
+            'music',
+            'graphic'
         ]
-    );
+    ];
+
+    // aggiungo un dato
+    $status = 'OK';
+
+    // response()->json() trasformerà l'array passante in un file JSON
+    // return response()->json($user);
+
+    // con compact() trasformerà l'array in un oggetto aggiungendo un livello in più
+    // return response()->json(compact('user'));
+
+    // posso anche passare più dati
+    return response()->json(compact('status', 'user'));
 });
