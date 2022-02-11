38dd8bef67544e9e2a20cb300f3d9b39aa03c291
andreafontaneto <andrea.fontaneto@gmail.com>
2022-02-11 17:45:07 +0100

    rename guest view in home + split css and js w/ webpack + npm run watch + re-link asset()

--- webpack.mix.js
+++ webpack.mix.js
@@ -11,5 +11,15 @@ const mix = require('laravel-mix');
  |
  */
 
-mix.js('resources/js/app.js', 'public/js')
-    .sass('resources/sass/app.scss', 'public/css');
+// separo gli stili e i javascript in base alle cartelle create (admin e guest)
+// da app.js creo 2 file js (admin e guest) e separo le dipendenze (Bootstrap in "admin" / Vue in "guest")
+// in sass creo 2 cartelle (admin e guest) ognuna con il suo style.scss (admin ha anche le _variables)
+// poi aggiungo le options per i background-img  
+
+mix.js('resources/js/admin.js', 'public/js')
+    .js('resources/js/guest.js', 'public/js')
+    .sass('resources/sass/admin/style.scss', 'public/css/admin')
+    .sass('resources/sass/guest/style.scss', 'public/css/guest')
+    .options({
+		processCssUrls: false
+	});
